6fb404f807c24dbaa0ea2fc54e64d9eb86a8d521
slonevskyi <jeksons11@gmail.com>
2020-03-09 22:43:45 +0200

    Added view response.

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,23 @@ var express = require('express');
 var router = express.Router();
 var app = express();
 var cors = require('cors')
+var http = require('http');
+var fs = require('fs');
 var mysql = require('../app.js');
 /* GET users listing. */
 
+router.get('/chat', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./views/index.html', null, function(error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('File not found!');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+});
 
 router.get('/', function (req, res) {
     mysql.mysqlcon.query(
