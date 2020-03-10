fec48b8767062736dbea05ea2c7c09a083d7c831
slonevskyi <jeksons11@gmail.com>
2020-03-10 12:36:39 +0200

    Enabled XHR.

--- routes/users.js
+++ routes/users.js
@@ -20,6 +20,14 @@ router.get('/chat', function (req, res) {
     });
 });
 
+router.post('/', function (req, res) {
+    mysql.mysqlcon.query(
+        'INSERT INTO `messages` (`sender_name`, `to_name`, `text_data`) VALUES ('+req.$name+',"all", '+req.$message+')', function (error, results, fields) {
+            if (error) throw error;
+            res.send(results);
+            //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        });
+});
 router.get('/', function (req, res) {
     mysql.mysqlcon.query(
         'SELECT * FROM messages', function (error, results, fields) {
