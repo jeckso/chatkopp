b4af09a70a08cef9b6b9491fa10142aaf7201b5c
slonevskyi <jeksons11@gmail.com>
2020-03-11 01:45:59 +0200

    Styling messages.

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ app.use(bodyparser.json())
 
 app.use('/api', usersRouter);
 
-
-var mysqlConnection = mysql.createConnection('mysql://b3020c234f7bf9:c2f9aeec@eu-cdbr-west-02.cleardb.net/heroku_a055cf7e4179e62?reconnect=true').connect(done);
-
+var mysqlConnection = mysql.createConnection('mysql://b3020c234f7bf9:c2f9aeec@eu-cdbr-west-02.cleardb.net/heroku_a055cf7e4179e62?reconnect=true');
+    mysqlConnection.connect();
 
 // var mysqlConnection = mysql.createConnection({
 //   host: "localhost",
