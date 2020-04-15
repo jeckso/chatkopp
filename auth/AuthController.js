fa14f69ddd8e4f122ffa05b3a1bedae7147e7b34
slonevskyi <jeksons11@gmail.com>
2020-04-15 23:52:45 +0300

    Changed DB and queries.

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -18,15 +18,15 @@ var config = require('../config'); // get config file
 router.post('/login', function (req, res) {
     var hashedPassword = bcrypt.hashSync(req.body.chat_pass, 8);
 
-    mysql.query('SELECT `chat_name`, `chat_pass` FROM `credentials` WHERE `chat_name` = "' + req.body.chat_name + '"', function (error, results, fields) {
+    mysql.query('SELECT `id` FROM `room` WHERE `name` = "' + req.body.chat_name + '"', function (error, results, fields) {
         if (error) return res.status(500).send("There was a problem logging in`.");
         if (results == 0) {
             return res.status(404).send("No chat with name ", req.body.chat_name, " found :(");
         } else {
 
-            var passwordIsValid = bcrypt.compareSync(req.body.chat_pass, results[0].chat_pass);
+            var passwordIsValid = bcrypt.compareSync(req.body.chat_pass, results[0].pass);
             if (!passwordIsValid) return res.status(401).send({auth: false, token: null});
-            var token = jwt.sign({id: req.body.chat_name}, config.secret, {
+            var token = jwt.sign({id: req.body.id}, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
             });
             res.status(200).send({auth: true, token: token});
@@ -58,21 +58,13 @@ router.post('/login', function (req, res) {
 router.get('/logout', function (req, res) {
     res.status(200).send({auth: false, token: null});
 });
-router.get('/:name', VerifyToken, function (req,res) {
-    mysql.query(
-        'SELECT * FROM "' + name + '" ORDER BY id DESC', function (error, results, fields) {
-            if (error) throw error;
-            res.send(results);
-            //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        });
 
-});
 
 
 router.post('/create', function(req,res){
     var hashedPassword = bcrypt.hashSync(req.body.chat_pass, 8);
-    mysql.query('INSERT INTO `credentials` (`chat_name`, `chat_pass`) VALUES ("' + req.body.chat_name + '", "' + hashedPassword + '")', function (err, user) {
-        if (err) return res.status(500).send("There was a problem creating chat`.");
+    mysql.query('INSERT INTO `room` (`name`, `pass`) VALUES ("' + req.body.chat_name + '", "' + hashedPassword + '")', function (err, user) {
+        if (err) return res.status(401).send("There was a problem creating chat`.");
         res.redirect(307, '/auth/login');
         //res.status(200).send("Chat created successfully!");
 
